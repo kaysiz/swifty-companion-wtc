0dbc36a1936140428577bb32844eece5a78c56de
Kudakwashe Siziva <ksiziva@gmail.com>
2019-10-09 09:22:23 +0200

    New App

---
+++ companion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  companion
+//
+//  Created by kudakwashe on 2019/10/09.
+//  Copyright © 2019 WeThinkCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
