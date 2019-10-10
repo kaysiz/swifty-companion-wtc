b7b05c8ce01f42e597b8d139479edae0f6ed9a70
Kudakwashe Siziva <ksiziva@gmail.com>
2019-10-10 06:20:07 +0200

    First screen working

--- companion/ViewController.swift
+++ companion/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var laError: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buSearch(_ sender: Any) {
+        laError.text = "*User not found!"
+    }
 }
 
